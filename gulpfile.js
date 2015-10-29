dbff2cd6ab4f318eb0396723380ca27a7b3327a8
Tolga Ekmen <tolga.ekmen@gmail.com>
2015-10-29 18:52:24 -0400

    Add clean and watch tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@
 
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
+var changed = require('gulp-changed');
+var del = require('del');
 
 gulp.task('minimize-images', function() {
   return gulp.src('img/**/*.gif')
+    .pipe(changed('build'))
     .pipe(imagemin())
     .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', function(callback) {
+  del('build/*', callback);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('img/**/*', ['minimize-images']);
 });
 
 gulp.task('default', ['minimize-images']);
